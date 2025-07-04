ef0d4dc0c71fbafe70819f765fa95dbbbc5445fb
kinhvi_pl <kinhvi0710@gmail.com>
2025-07-04 00:34:27 -0400

    Add database models

--- backend/src/routes/routes-user.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { getAllUsers } from '../controllers/controller-user.js';
-
-const userRouter = Router();
-
-/*---------------------------Routes---------------------------*/
-userRouter.get("/", getAllUsers);
-export default userRouter;
