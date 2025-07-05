96f1746dc67325aeaae0459edd8defe893d9b1eb
kinhvi_pl <kinhvi0710@gmail.com>
2025-07-05 17:37:24 -0400

    Edit nav bar

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import React from 'react';
+import { AppBar, Toolbar } from '@mui/material';
+import Logo from './shared/Logo';
+import NavLink from './shared/NavLink';
+import { useAuth } from '../context/AuthContext';
 
 const Header = () => {
+  const auth = useAuth();
+  
+  
+  
   return (
-    <div>Header</div>
-  )
+    <AppBar 
+    sx={{
+      bgcolor: "transparent", 
+      position: "static", 
+      boxShadow: "none"}}>
+      <Toolbar 
+      sx={{
+        display: "flex"
+        }}>
+          <Logo />
+          <div>
+            {auth?.isLoggedIn ? (
+              <>
+                <NavLink 
+                  bg="#00fffc"
+                  textColor='black'
+                  to="/chat"
+                  text="Go to chat"                  
+                  />
+
+                <NavLink 
+                  bg="#51538f"
+                  textColor='white'
+                  to='/'
+                  text="Log out"
+                  onClick={auth.logout}
+                  />
+              </>
+            ) : (
+              <>
+                <NavLink 
+                  bg="#00fffc"
+                  textColor='black'
+                  to="/login"
+                  text="Log in"                  
+                  />
+
+                <NavLink 
+                  bg="#51538f"
+                  textColor='white'
+                  to='/signup'
+                  text="Sign up"
+                  />
+              </>
+            )}
+          </div>
+        </Toolbar>
+    </AppBar>
+    
+    
+  );
 }
 
 export default Header
